ede15988d2cc1d04cc168f1e359f0bdf91a281f4
grimharald <grimharald@mailfence.com>
2026-01-20 10:26:23 +0000

    fix bibidog

---
+++ bibidawg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+INPUT_FILE="kjv.txt"
+
+# Read the file line by line
+while IFS= read -r line
+do
+  echo -e "\n $line\n" | boxes -d dog|lolcat
+  
+  sleep 2
+
+done < "$INPUT_FILE"
+
